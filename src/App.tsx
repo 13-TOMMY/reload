f94467841624e2b609e668dedf2a0fad50170f3f
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-04 09:32:21 +0200

    vite/ts  added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import InternetSpeed from './Components/InternetSpeed/InternetSpeed';
+
+function App() {
+  
+
+  return <>
+    <InternetSpeed />
+    </>
+}
+
+export default App;
---
+++ src/Components/InternetSpeed/InternetSpeed.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./InternetSpeed.css"; // Make sure this path is correct
+
+const InternetSpeed: React.FC = () => {
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default InternetSpeed;
